9ec5ae7957543cb1077f5619b06079bd5bd8eab9
VisanCosmin <cozma.cozmyn@yahoo.com>
2021-07-31 17:40:48 +0300

    todo useState

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState , useEffect } from 'react';
 
 function App() {
+  const [input,setInput] = useState("");
+  const [todos,setTodos] = useState([]);
+  const [filteredTodos,setFilteredTodos] = useState([]);
+  const [filter,setFilter] = useState("all");
+
+
+  const handleInput = (event) => {
+    if(event.key === "Enter"){
+      const newTodo = { done : false 
+                      , value : event.target.value 
+                      };
+      setTodos( todos.concat([newTodo]) );
+
+      setInput("");
+    }
+  };
+
+  useEffect( () => {
+      switch(filter){
+        case "active":{
+          setFilteredTodos(todos.filter( todo => !todo.done ));
+        }break;
+        case "completed":{
+          setFilteredTodos(todos.filter( todo => todo.done ));
+        }break;
+        default:{
+          setFilteredTodos(todos);
+        }
+      }
+  } , [todos,filter] );
+
+  const toggleTodo = (id) => (val) => {
+    const newTodos = [...todos];
+    newTodos[id].done = val;
+    setTodos(newTodos);
+  }
+
+  const clearCompleted = () => {
+    setTodos(todos.filter(todo => !todo.done));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <h2>Todo App</h2>
+      <input type="text" value={input} onKeyPress={handleInput} onChange={e => setInput(e.target.value)} />
+      <ul>
+        {
+          filteredTodos.map( (todo,key) => <TodoElement todo={todo} id={key} toggleTodo={toggleTodo(key)} /> )
+        }
+      </ul>
+      { todos.filter(todo => !todo.done).length > 1 && <p>{todos.filter(todo => !todo.done).length} items left</p> }
+      { todos.filter(todo => !todo.done).length === 1 && <p>1 item left</p> }
+      <div className="buttons">
+        <button value="all" 
+                onClick={ () => setFilter("all") } 
+                className={filter === "all" && "selected"} >All</button>
+        <button value="active" 
+                onClick={ () => setFilter("active") } 
+                className={filter === "active" && "selected"} >Active</button>
+        <button value="completed" 
+                onClick={ () => setFilter("completed") } 
+                className={filter === "completed" && "selected"} >Completed</button>
+
+        <button onClick={clearCompleted} className="clear">Clear completed</button>
+      </div>
     </div>
+  );
+}
+
+function TodoElement(props){
+  return (
+    <li id={props.id} key={props.id}>
+      <input type="checkbox" checked={props.todo.done} onChange={e => props.toggleTodo(e.target.checked)}/>
+      {props.todo.done === true ? <s>{props.todo.value}</s> : props.todo.value}
+    </li>
   );
 }
 
